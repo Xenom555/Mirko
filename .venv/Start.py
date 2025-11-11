6a8237106ac413b3bba77988334515a1852c7d4a
xenom <xenom444@gmail.com>
2025-11-11 21:18:27 +0100

    initial

---
+++ .venv/Start.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import filedialog
+import pandas as PD
+
+
+def open_file():
+    file_path = filedialog.askopenfilename(
+        title="1st File",
+        filetypes=(("Text files", "*.*"), ("All files", "*.*"))
+    )
+    if file_path:
+        print(f"File selected: {file_path}")
+        df = PD.read_excel(file_path)
+        print(df.head())
+
+
+
+root = tk.Tk()
+root.withdraw()  # Hide the main window
+open_file()
