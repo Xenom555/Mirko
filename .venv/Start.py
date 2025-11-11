93933a0112d98a31d3d500cc35af0000df56fea8
xenom <xenom444@gmail.com>
2025-11-11 21:32:36 +0100

    Update

--- .venv/Start.py
+++ .venv/Start.py
@@ -1,25 +1,26 @@
 import tkinter as tk
 from tkinter import filedialog
 import pandas as PD
-
+from rapidfuzz import fuzz
 
 def open_file():
-    file_path = filedialog.askopenfilename(
+    file_path_1 = filedialog.askopenfilename(
         title="1st File",
         filetypes=(("Text files", "*.*"), ("All files", "*.*"))
     )
-    file_path = filedialog.askopenfilename(
+    file_path_2 = filedialog.askopenfilename(
         title="2nd File",
         filetypes=(("Text files", "*.*"), ("All files", "*.*"))
     )
-    if file_path:
-        print(f"File selected: {file_path}")
-        df = PD.read_excel(file_path)
-        print(df.head())
+    if file_path_1:
+        print(f"File selected: {file_path_1}")
+        df1 = PD.read_excel(file_path_1)
+        print(df1.head())
 
-
-
-
+    if file_path_2:
+        print(f"File selected: {file_path_2}")
+        df2 = PD.read_excel(file_path_2)
+        print(df2.head())
 
 root = tk.Tk()
 root.withdraw()  # Hide the main window
